aee315c558d91faf8f8d7fbe228348b8a723a5c4
ImJonathan365 <jonathan365.git@gmail.com>
2026-05-01 17:35:26 -0600

    Endpoints de prueba

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 
+from app.api.v1.router import api_router
+
 
 app = FastAPI()
+app.include_router(api_router, prefix="/api/v1")
 
 
 @app.get("/")
 def hello_world():
-	return "Hola mundo"
+    return "Hola mundo"
